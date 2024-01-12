ec2b39f411b700d2898a1b409b65b3984783a044
isaul19 <saulhola348@gmail.com>
2024-01-11 19:14:34 -0500

    add clerk, add amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 export const App = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <RouterProvider router={router} />
+      </ClerkProvider>
     </>
   );
 };
