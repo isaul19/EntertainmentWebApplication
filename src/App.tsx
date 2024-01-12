1ea2718e6192439b93f24d293a70a2c148a66084
isaul19 <saulhola348@gmail.com>
2024-01-11 19:59:20 -0500

    remove clerk dependencie

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { ClerkProvider } from "@clerk/clerk-react";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 
 export const App = () => {
   return (
     <>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <RouterProvider router={router} />
-      </ClerkProvider>
+      <RouterProvider router={router} />
     </>
   );
 };
